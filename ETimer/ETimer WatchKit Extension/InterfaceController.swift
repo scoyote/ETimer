d545847b751f34fe749fd4d905411e31b4e39224
Sam Croker(MacBook) <samuel.croker@gmail.com>
2017-06-25 09:38:58 -0400

    Initial build that shows only basic fcn.

--- ETimer/ETimer WatchKit Extension/InterfaceController.swift
+++ ETimer/ETimer WatchKit Extension/InterfaceController.swift
@@ -27,9 +27,76 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-    @IBOutlet var label: WKInterfaceLabel!
-    @IBAction func buttonTapped() {
-        label.setText("Hi Hello Hi")
+    
+    // Keep track of the time the workout started.
+    var workoutStartTime: Date?
+
+    @IBOutlet var workoutTimer: WKInterfaceTimer!
+    
+    @IBAction func startWorkout() {
+        // To count up use 0.0 or less, otherwise the timer counts down.
+        workoutTimer.setDate(Date(timeIntervalSinceNow: 0.0))
+        workoutTimer.start()
+        self.workoutStartTime = Date()
     }
+
+    @IBAction func stopWorkout() {
+        workoutTimer.stop()
+    }
+
+        /*
+    let seconds:Int64
+    
+    seconds         = 60
+    var timer           = Timer()
+    var isTimerRunning  = false
+    var resumeTapped    = false
+    
+    // this is sams code:
+    @IBOutlet var timerLabel: WKInterfaceLabel!
+    
+
+    @IBAction func startButtonTapped() {
+        timer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: (#selector(InterfaceController.updateTimer)),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+
+    @IBAction func pauseButtonTapped() {
+        if self.resumeTapped == false {
+            timer.invalidate()
+            self.resumeTapped = true
+        } else {
+            runTimer()
+            self.resumeTapped = false
+        }
+    }
+    
+    @IBAction func resetButtonTapped() {
+        timer.invalidate()
+        seconds = 60    //Here we manually enter the restarting point for the seconds, but it would be wiser to make this a variable or constant.
+        timerLabel.setText("\(seconds)")
+    }
+ 
+    func runTimer() {
+        timer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: (#selector(InterfaceController.updateTimer)),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+ 
+    func updateTimer() {
+        seconds += 1     //This will decrement(count down)the seconds.
+
+        timerLabel.setText("\(seconds)") //This will update the label.
+    }
+*/
     
 }
